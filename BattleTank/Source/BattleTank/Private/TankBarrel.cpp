02e39c29a3ca94db2834b1a998ad190798574a74
Targeir Attestog <attestog@gmail.com>
2018-02-10 22:03:22 +0100

    Adjustments in code review.

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -2,12 +2,10 @@
 
 #include "TankBarrel.h"
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move the barrel the right amout this frame	
 	//given the max elevation speed and the frame time.
-	UE_LOG(LogTemp, Warning, TEXT("Barrel->Elevate() called at speed %f"), DegreesPerSecond);
-
 }
 
 
